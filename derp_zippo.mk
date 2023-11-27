812ec0086a1b06598179b24a3d524d917f1b3279
Einar Gednochsson <pete80pro.pb@gmail.com>
2023-11-27 10:40:57 +0100

    zippo: Derpfest Official

--- derp_zippo.mk
+++ derp_zippo.mk
@@ -1,20 +1,26 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2023 The Derpfest Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+DERP_BUILDTYPE := Official
+USE_LEGACY_BOOTANIMATION := true
+DERP_VERSION_APPEND_TIME_OF_DAY := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo L78051
-PRODUCT_NAME := lineage_zippo
+PRODUCT_NAME := derp_zippo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
